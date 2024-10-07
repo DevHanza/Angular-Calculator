ee419ebe5bb419bf1f53356f50b59d85b9ec278a
Hansana Dasanayaka <75363593+DevHanza@users.noreply.github.com>
2024-10-08 04:37:58 +0530

    Added button events (onclick event)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-calculator';
+  title = 'Angular Calculator';
+
+  calValue: number = 0;
+  funcT: any = 'NoFunction';
+
+  onClickValue(val: string, type: any) {
+    console.log(val, type);
+  }
 }
+
+// https://www.youtube.com/watch?v=EuJsvyFz2kA&t=33m31s
